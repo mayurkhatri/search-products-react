f29a0d8ef1856b32b907763c72e263345b0651a0
mayurkhatri <mayurkhatri122@gmail.com>
2018-01-25 16:36:47 +0530

    Search bar - add checkbox implementation

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const PRODUCTS = [{category: "VCD", price: "Rs. 100", stocked: true, name: "Latest Movie"}, {category: "VCD", price: "Rs. 100", stocked: true, name: "Yoga"}, {category: "VCD", price: "Rs. 100", stocked: true, name: "Fitness"},
+const PRODUCTS = [{category: "VCD", price: "Rs. 100", stocked: false, name: "Latest Movie"}, {category: "VCD", price: "Rs. 100", stocked: true, name: "Yoga"}, {category: "VCD", price: "Rs. 100", stocked: true, name: "Fitness"},
 {category: "Electronics", price: "Rs. 3000", stocked: true, name: "Fast track watch"}, {category: "Electronics", price: "Rs. 10000", stocked: true, name: "LG washing machine"}]
 
 class App extends Component {
@@ -14,19 +14,24 @@ class App extends Component {
 class FilterableProductTable extends Component {
   constructor(props){
     super(props);
-    this.state = {searchText: ''};
+    this.state = {searchText: '', onlyProductsInStock: false};
     this.handleSearchTextChange = this.handleSearchTextChange.bind(this);
+    this.handleIsStockedChange = this.handleIsStockedChange.bind(this);
   }
 
   handleSearchTextChange(searchText){
     this.setState({searchText: searchText});
   }
 
+  handleIsStockedChange(onlyProductsInStock){
+    this.setState({onlyProductsInStock: onlyProductsInStock});
+  }
+
   render() {
     return (
       <div>
-        <SearchBar searchText={this.state.searchText} onSearchTextChange={this.handleSearchTextChange}/>
-        <ProductTable products={PRODUCTS}/>
+        <SearchBar searchText={this.state.searchText} onProductsInStockChange={this.handleIsStockedChange} onSearchTextChange={this.handleSearchTextChange}/>
+        <ProductTable products={PRODUCTS} searchText={this.state.searchText} onlyProductsInStock={this.state.onlyProductsInStock} />
       </div>
     );
   }
@@ -36,19 +41,25 @@ class SearchBar extends Component {
   constructor(props){
     super(props);
     this.handleSearchTextChange = this.handleSearchTextChange.bind(this);
+    this.handleIsStockedChange = this.handleIsStockedChange.bind(this);
   }
 
   handleSearchTextChange(e){
     this.props.onSearchTextChange(e.target.value);
   }
 
+  handleIsStockedChange(e){
+    this.props.onProductsInStockChange(e.target.checked);
+  }
+
   render() {
     const searchText = this.props.searchText;
+    const onlyProductsInStock = this.props.onlyProductsInStock;
     return(
       <form>
         <input id="searchInput" type="text" placeholder="Search..."  value={searchText} onChange={this.handleSearchTextChange}></input>
         <p>
-          <input type="checkbox" />
+          <input type="checkbox" checked={onlyProductsInStock} onChange={this.handleIsStockedChange} />
           {' '}
           Only show products in stock
         </p>
@@ -61,7 +72,8 @@ class ProductTable extends Component {
   render() {
     var rows = [];
     var iteratedCategories = [];
-    //rows = this.props.products.map((product, index) => (<tr><td>{product.name}</td><td>{product.price}</td></tr>));
+    const searchText = this.props.searchText;
+    const onlyProductsInStock = this.props.onlyProductsInStock;
     this.props.products.forEach((product) => {
       if(iteratedCategories.indexOf(product.category) === -1){
         iteratedCategories.push(product.category);
@@ -69,10 +81,17 @@ class ProductTable extends Component {
           <ProductCategoryRow category={product.category} key={product.category} />
         );
       }
+      if(product.name.indexOf(searchText) === -1) {
+        return;
+      }
+      if(onlyProductsInStock){
+        if(product.stocked !== onlyProductsInStock){
+          return;
+        }
+      }
       rows.push(
         <ProductRow product={product} key={product.name} />
       );
-
     });
 
     return (
